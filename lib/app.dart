3d501cb0686638667ea7af1d35ada9ca22f41cdf
prashantdahal1 <prashantdahal319@gmail.com>
2026-05-19 13:34:17 +0545

    Initial commit - completely clean

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+    );
+  }
+}
